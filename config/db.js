7421b2346f834c59e66e4796bcaf0d569102384b
Ahmad Soboh <asobo058@uottawa.ca>
2024-09-26 23:17:44 -0400

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 require('dotenv').config();
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-=======
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://ahmadsoobh:Intel777!@carinfo.9qs8w.mongodb.net/?retryWrites=true&w=majority&appName=CarInfo', {
->>>>>>> ceb0472a225c0282756c5864017ddabf81caa17b
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
